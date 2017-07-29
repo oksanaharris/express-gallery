a5dfac7b2fcd983058d84414b2d10c360f51616e
Oksana Harris <oksanaharris@Oksanas-MacBook-Pro.local>
2017-07-28 16:14:06 -1000

    set up sequelize

---
+++ models/images.js
@@ -0,0 +1,15 @@
+
+
+module.exports = function (sequelize, DataTypes){
+  var Images = sequelize.define("Images", {
+    url: DataTypes.STRING,
+    description: DataTypes.TEXT
+  });
+  Images.associate = function(models) {
+    Images.belongsTo(models.Authors, {foreignKey: 'authors_id'});
+  }
+
+  return Images;
+};
+
+
--- server.js
+++ server.js
@@ -6,7 +6,19 @@ var app = express();
 var PORT = process.env.PORT || 9000;
 var bodyParser = require('body-parser');
 
+app.use(bodyParser.urlencoded({extended: true}));
 
+let db = require('./models');
+
+let Images = db.Images;
+let Authors = db.Authors;
+
+app.get('/images', function(req, res) {
+  Images.findAll()
+  .then(function (images){
+    res.json(images);
+  });
+});
 
 
 app.get('/', (req, res)=>{
@@ -14,21 +26,8 @@ app.get('/', (req, res)=>{
 });
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 app.listen(PORT, ()=>{
+  db.sequelize.drop();
+  db.sequelize.sync();
   console.log(`server running on ${PORT} `);
 });
